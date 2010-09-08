5f04d52160bf429f26b25031486648533a0ad133
Samuel O. Obukwelu <sam@fcgmedia.com>
2010-09-08 06:51:53 -0400

    removed validation constants

--- lib/fcg-core-ext.rb
+++ lib/fcg-core-ext.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'fcg-core-ext/version'
 require "fcg-core-ext/string"
 require "fcg-core-ext/integer"
@@ -5,4 +6,3 @@ require "fcg-core-ext/hash"
 require "fcg-core-ext/array"
 require "fcg-core-ext/time_utils"
 require "fcg-core-ext/utils"
-require "fcg-core-ext/validation_constants"
--- lib/fcg-core-ext/utils.rb
+++ lib/fcg-core-ext/utils.rb
@@ -1,6 +1,12 @@
 module FCG
   module CoreExt
     module Utils
+      ::BAD_USERNAMES = %w(user users mail login logout new destroy create edit admin ssl xxx sex bitch bitches admin hoe hoes)
+      ::REGEX = {
+        :username => /^([a-zA-Z0-9_-]{3,16})$/,
+        :email => /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i
+      }
+      # email regex from http://fightingforalostcause.net/misc/2006/compare-email-regex.php
       def time_string(date)
         date.strftime("%I:%M%p").sub(/^0/,'')
       end
@@ -30,7 +36,7 @@ module FCG
         render :json => objekt
       end
   
-      def log(msg)
+      def log(msg, )
         Rails.logger.debug  "### #{msg}"
       end
   
