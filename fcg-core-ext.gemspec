16ffdddff5f66b6ef8ba06d5d79ab2a2d8683ff6
Samuel O. Obukwelu <sam@fcgmedia.com>
2010-09-09 08:50:02 -0400

    fixed regex constant

--- fcg-core-ext.gemspec
+++ fcg-core-ext.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Samuel O. Obukwelu"]
-  s.date = %q{2010-09-08}
+  s.date = %q{2010-09-09}
   s.description = %q{Ruby classes and methods altered}
   s.email = %q{sam@fcgmedia.com}
   s.extra_rdoc_files = [
--- lib/fcg-core-ext/utils.rb
+++ lib/fcg-core-ext/utils.rb
@@ -3,7 +3,7 @@ module FCG
     module Utils
       ::BAD_USERNAMES = %w(user users mail login logout new destroy create edit admin ssl xxx sex bitch bitches admin hoe hoes)
       ::REGEX = {
-        :username => /^([a-zA-Z0-9_-]{3,16})$/,
+        :username => /^([a-z0-9][a-z0-9_-]{2,15})$/i,
         :email => /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i
       }
       # email regex from http://fightingforalostcause.net/misc/2006/compare-email-regex.php
