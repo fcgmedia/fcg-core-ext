df083e2f093f3feabd94f842f1cd818f6697ae2b
Samuel O. Obukwelu <sam@fcgmedia.com>
2011-06-21 08:08:01 -0400

    removed msgpack and facets

--- lib/fcg-core-ext.rb
+++ lib/fcg-core-ext.rb
@@ -4,8 +4,8 @@ require 'yajl'
 require 'yajl/json_gem'
 require 'andand'
 require 'hashie'
-require 'msgpack'
-require 'facets'
+# require 'msgpack'
+# require 'facets'
 include Hashie::HashExtensions
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
